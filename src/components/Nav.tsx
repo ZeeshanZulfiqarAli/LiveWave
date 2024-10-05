6ee76a0b2bbf38b6959948353fb1d7e80bf2123b
waleedbinalim <waleedbinalim@gmail.com>
2024-10-05 14:50:08 +0500

    init commit x2

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { useLocation } from "@solidjs/router";
 
 export default function Nav() {
   const location = useLocation();
-  const active = (path) =>
+  const active = (path: string) =>
     path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
   return (
     <nav class="bg-sky-800">
