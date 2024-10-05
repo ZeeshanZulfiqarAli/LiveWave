297a5582dfad239729b17dfe477f410c7ddc50e9
Zeeshan <zeeshan.dossani23@gmail.com>
2024-10-05 02:01:03 -0500

    initial commit

---
+++ src/routes/[...404].jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        Man, Waleed didn't work on this?!?!
+      </h1>
+    </main>
+  );
+}
