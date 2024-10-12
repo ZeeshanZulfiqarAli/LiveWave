570becec681257760d1d8d7f9c2d1c5bf8ce99c0
waleedbinalim <waleedbinalim@gmail.com>
2024-10-12 18:46:39 +0500

    chore: ui fix

--- src/routes/prepare-to-party.tsx
+++ src/routes/prepare-to-party.tsx
@@ -20,35 +20,46 @@ const PrepareToPartyPage = () => {
   return (
     <>
       <div class="min-h-screen bg-gradient-to-bl from-cyan-400 to-blue-900 flex">
-        <div class="flex flex-col gap-2 w-1/2 p-6 justify-center">
-          <input
-            ref={(el) => (inputRef = el)}
-            class="w-1/2 rounded-md min-h-12 px-4 py-2 text-black"
-            placeholder="Username"
-            type="text"
-            onKeyUp={() => {
-              setMyUserStore("username", inputRef.value.trim());
-            }}
-          />
+        <div class="w-1/2 p-6 grid place-content-center">
+          <div class="bg-slate-50 flex flex-col gap-2 p-12 pt-0 rounded-xl drop-shadow-lg shadow-2xl">
+            <h3 class="font-bold text-xl text-center pt-6 pb-4">
+              Create Your Profile
+            </h3>
 
-          <div class="w-1/2">
-            <CommonSelect
-              options={avatarDropdownOptions}
-              onChange={handleAvatarChange}
-              placeholder="Select Character"
-            />
-          </div>
+            <div class="flex flex-col gap-4">
+              <input
+                ref={(el) => (inputRef = el)}
+                class="w-full drop-shadow-sm border border-black text-black rounded-md min-h-12 px-4 py-2"
+                placeholder="Username"
+                type="text"
+                onKeyUp={() => {
+                  setMyUserStore("username", inputRef.value.trim());
+                }}
+              />
 
-          <div class="w-1/2">
-            <CommonSelect
-              options={userColorOptions}
-              onChange={handleFrameChange}
-              placeholder="Select Color"
-            />
-          </div>
+              <div class="w-full drop-shadow-sm border border-black rounded-md">
+                <CommonSelect
+                  options={avatarDropdownOptions}
+                  onChange={handleAvatarChange}
+                  placeholder="Select Character"
+                />
+              </div>
 
-          <div>
-            <input type="file" />
+              <div class="w-full drop-shadow-sm border border-black rounded-md">
+                <CommonSelect
+                  options={userColorOptions}
+                  onChange={handleFrameChange}
+                  placeholder="Select Color"
+                />
+              </div>
+            </div>
+
+            <div>
+              <h3 class="text-sm text-slate-800 mt-4 mb-2">
+                Optional: Bring your song to the party
+              </h3>
+              <input class="text-sm" type="file" />
+            </div>
           </div>
         </div>
 
